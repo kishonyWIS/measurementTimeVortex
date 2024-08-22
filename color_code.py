fix calculation of bond position for delay, fix generation of edge plaquettes.

color_code.py
@@ -58,7 +58,7 @@ class FloquetCode:
         return site[0] / self.num_sites_x * self.num_vortexes[0] + site[1] / self.num_sites_y * self.num_vortexes[1]
 
     def get_bond_order(self, sites, color, pauli_label):
-        site_midpoint = np.mean(sites, axis=0)
+        site_midpoint = np.mean(self.geometry.sites_unwrap_periodic(sites), axis=0)
         order_without_vortex = (-color / 3 + (pauli_label == 'ZZ') / 2) % 1
         order = order_without_vortex + self.location_dependent_delay(site_midpoint)
         order = order % 1
@@ -227,7 +227,7 @@ class FloquetCode:
         fig, ax = plt.subplots(figsize=(15, 10))
 
         for plaquette in self.plaquettes:
-            if plaquette.pauli_label == 'X':
+            if plaquette.pauli_label == 'Z':
                 continue
             sites, was_shifted = self.geometry.sites_unwrap_periodic(plaquette.sites, return_was_shifted=True)
             if was_shifted:

color_code_thershold_draw_graph.py
@@ -49,7 +49,7 @@ linestyle_column = 'num_vortexes'  # This can be 'vortex_location' or another co
 marker_column = 'logical_operator_direction'  # This can be 'logical_operator_direction' or another column
 
 # # Filter the DataFrame to only include X logical operators and vortex_sign = '1'
-df = df.query('geometry == "SymmetricTorus"')
+df = df.query('geometry == "SymmetricCylinder"')
 
 
 for logical_operator_direction in ['x', 'y', 'both']:
@@ -95,6 +95,7 @@ for logical_operator_direction in ['x', 'y', 'both']:
     edit_graph('Physical Error Rate', 'Logical Error Rate',
                scale=1.5)
 
+plt.show()
     # Show the plot
 # plt.savefig('figures/threshold.pdf')
 
@@ -105,7 +106,8 @@ df['num_vortexes_x'] = df['num_vortexes'].apply(lambda x: x[0])
 df['num_vortexes_y'] = df['num_vortexes'].apply(lambda x: x[1])
 df = df.drop(columns='num_vortexes')
 
-df_temp = df.query('dx == 9 and dy == 9')
+# take the maximum system size
+df_temp = df.query(f'dx == {max(df.dx)} and dy == {max(df.dy)}')
 
 # pivot the DataFrame to have the vortex configurations as the index
 for logical_operator_direction in ['x', 'y', 'both']:
@@ -120,4 +122,7 @@ for logical_operator_direction in ['x', 'y', 'both']:
     plt.colorbar(c, label='logical error rate'+' '+logical_operator_direction)
     plt.xlabel('Number of Y Vortexes')
     plt.ylabel('Number of X Vortexes')
+    # set the ticks to the values of the vortexes
+    ax.set_xticks(np.unique(x))
+    ax.set_yticks(np.unique(y))
 plt.show()

geometry.py
@@ -30,6 +30,8 @@ class Plaquette:
     def get_bonds(self, all_bonds):
         bonds = []
         for bond in all_bonds:
+            if len(bond.pauli_label) == 1 and len(self.sites) == 6:
+                continue
             if np.all([np.all(s == self.sites, axis=1).any() for s in bond.sites]):
                 if self.pauli_label is None or np.all([p == self.pauli_label for p in bond.pauli_label]):
                     bonds.append(bond)
@@ -85,10 +87,10 @@ class Geometry:
         shifted_sites = []
         for site in sites:
             new_site = copy(site)
-            if np.linalg.norm(site[0] - reference_site[0]) > 1:
+            if np.linalg.norm(site[0] - reference_site[0]) > 1 and self.boundary_conditions[0] == 'periodic':
                 new_site = new_site + np.array(
                     [self.num_sites_x * round((reference_site[0] - site[0]) / self.num_sites_x), 0, 0])
-            if np.linalg.norm(site[1] - reference_site[1]) > 1:
+            if np.linalg.norm(site[1] - reference_site[1]) > 1 and self.boundary_conditions[1] == 'periodic':
                 new_site = new_site + np.array(
                     [0, self.num_sites_y * round((reference_site[1] - site[1]) / self.num_sites_y), 0])
             shifted_sites.append(new_site)
@@ -194,7 +196,7 @@ class SymmetricCylinder(Geometry):
         if direction == 'x':
             return [[ix, iy, s]
                     for ix in range(self.num_sites_x)
-                    for iy in [0]
+                    for iy in [1]
                     for s in [0, 1]]
         elif direction == 'y':
             return [[ix, iy, s]