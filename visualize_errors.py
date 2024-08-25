e49737f3090d56e30151f81e9803b933f6427abe
shohamjac <51854221+shohamjac@users.noreply.github.com>
2024-08-25 16:54:40 +0300

    visualize all errors

--- visualize_errors.py
+++ visualize_errors.py
@@ -1,3 +1,5 @@
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
 from color_code import *
 from geometry import *
 from noise import get_noise_model
@@ -43,9 +45,19 @@ def draw_pauli_3d(code, ax=None, z_plane=0):
 
 
 
-def draw_shortest_error(circ: stim.Circuit):
+def draw_shortest_error(circ: stim.Circuit, draw_graph=False):
     fig = plt.figure()
     ax = fig.add_subplot(projection = '3d')
+    if draw_graph:
+        dem = circ.detector_error_model()
+        for dem_element in dem:
+            if dem_element.type == 'error' and len(dem_element.targets_copy()) == 2:
+                det1 = dem_element.targets_copy()[0].val
+                det2 = dem_element.targets_copy()[1].val
+                cord1 = dem.get_detector_coordinates(det1)[det1]
+                cord2 = dem.get_detector_coordinates(det2)[det2]
+                ax.plot([cord1[0], cord2[0]], [cord1[1], cord2[1]], zs=[cord1[2], cord2[2]],
+                        color='r', linewidth=1)
     for error in circ.shortest_graphlike_error():
         coords = [detector.coords[:3] for detector in error.dem_error_terms]
         coords = [c for c in coords if len(c)>0]
@@ -56,7 +68,7 @@ def draw_shortest_error(circ: stim.Circuit):
     plt.ylabel('y')
     ax.set_zlabel('time')
     corner = code.geometry.site_to_physical_location([dx,dy,2])
-    ax.set_box_aspect([corner[0]/corner[1], 1, 0.2])  # x:y:z ratio
+    ax.set_box_aspect([corner[0]/corner[1], 1, 1])  # x:y:z ratio
     draw_pauli_3d(code, ax=ax, z_plane=min(coords[:,2]))
 
 
@@ -69,7 +81,7 @@ if __name__ == '__main__':
     num_vortexes = (0, 1)  # (0,1)
     dx, dy = 12, 6
     reps_without_noise = 1
-    reps_with_noise = 12
+    reps_with_noise = 1
     noise_type = 'DEPOLARIZE1'
 
     code = FloquetCode(dx, dy, boundary_conditions=boundary_conditions,
@@ -83,6 +95,32 @@ if __name__ == '__main__':
         detector_indexes=None, detector_args=None)
 
     print(len(circ.shortest_graphlike_error()))
-    draw_shortest_error(circ)
+    draw_shortest_error(circ, draw_graph=False)
+    # plt.axis('equal')
+    plt.show()
+
+    boundary_conditions = ('periodic', 'periodic')
+    geometry = SymmetricTorus
+    logical_op_directions = ('x')
+    detectors = ('Z',)
+    logical_operator_pauli_type = 'Z'
+    num_vortexes = (0, -1)  # (0,1)
+    dx, dy = 12, 6
+    reps_without_noise = 1
+    reps_with_noise = 1
+    noise_type = 'DEPOLARIZE1'
+
+    code = FloquetCode(dx, dy, boundary_conditions=boundary_conditions,
+                       num_vortexes=num_vortexes, geometry=geometry, detectors=detectors)
+
+    circ, _, _ = code.get_circuit(
+        reps=reps_with_noise + 2 * reps_without_noise, reps_without_noise=reps_without_noise,
+        noise_model=get_noise_model(noise_type, 0.1),
+        logical_operator_pauli_type=logical_operator_pauli_type,
+        logical_op_directions=logical_op_directions,
+        detector_indexes=None, detector_args=None)
+
+    print(len(circ.shortest_graphlike_error()))
+    draw_shortest_error(circ, draw_graph=False)
     # plt.axis('equal')
     plt.show()
